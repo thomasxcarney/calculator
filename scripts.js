c7970360ca63121d60fb6eaf724f076c23578e13
thomasxcarney <thomas.carney26@gmail.com>
2023-07-28 08:58:57 -0400

    Create operate function and declare variables
    Variables have been declared for the two numbers and the operator in
    a basic math equation. The function operate has been created to take in
    those variables and return the correct answer.

--- scripts.js
+++ scripts.js
@@ -5,3 +5,19 @@ const subtract = (a, b) => (a-b);
 const multiply = (a, b) => (a*b);
 
 const divide = (a, b) => (a/b);
+
+let firstNumber;
+let secondNumber;
+let operator;
+
+function operate(firstNumber, operator, secondNumber){
+    if(operator == '+'){
+        return add(firstNumber, secondNumber);
+    } else if (operator == '-'){
+       return subtract(firstNumber, secondNumber);
+    } else if (operator == '*'){
+       return multiply(firstNumber, secondNumber);
+    } else if (operator == '/'){
+       return divide(firstNumber, secondNumber);
+    } else return "Error";
+};
